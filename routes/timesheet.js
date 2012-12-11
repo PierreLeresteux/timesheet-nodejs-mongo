c3433300884f659666201d02dd40f8a70a775bce
Pierre Leresteux <pierre.leresteux@gmail.com>
2012-12-11 18:09:24 +0100

    change MONGOLAB_URI

--- routes/timesheet.js
+++ routes/timesheet.js
@@ -4,7 +4,16 @@ var Server = mongo.Server,
 	Db = mongo.Db,
 	BSON = mongo.BSONPure;
 
+var db;
+
+var mongoUri = process.env.MONGOLAB_URI ||  'mongodb://localhost:27017/timesheetdb';
+
+mongo.Db.connect(mongoUri, function (err, database) {
+	db = database;
+});
+/*
 db = new Db('timesheetdb', new Server('localhost', 27017), {safe:false});
+
 
 db.open(function(err, db) {
 	if(!err) {
@@ -18,7 +27,7 @@ db.open(function(err, db) {
 		});
 	}
 });
-
+*/
 exports.findById = function(req, res) {
 	var id = req.params.id;
 	console.log('Retrieving timesheet: ' + id);
